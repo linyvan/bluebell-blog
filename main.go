dc09b725f2c397eafa441967871828b73919e35e
xiaoG <linxianyv@qq.com>
2023-03-07 11:47:05 +0800

    v1.2

--- main.go
+++ main.go
@@ -9,16 +9,19 @@ import (
 	"bluebell/router"
 	"bluebell/setting"
 	"fmt"
-	"os"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("need config file.eg: bluebell config.yaml")
-		return
-	}
-	// 加载配置
-	if err := setting.Init(os.Args[1]); err != nil {
+	//if len(os.Args) < 2 {
+	//	fmt.Println("need config file.eg: bluebell config.yaml")
+	//	return
+	//}
+	//// 加载配置
+	//if err := setting.Init(os.Args[1]); err != nil {
+	//	fmt.Printf("load config failed, err:%v\n", err)
+	//	return
+	//}
+	if err := setting.Init("conf/config.yaml"); err != nil {
 		fmt.Printf("load config failed, err:%v\n", err)
 		return
 	}
